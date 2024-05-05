f19ca48cdc1c13f39561a353fcd5d59a6c837b4e
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-05-05 17:56:31 +0000

    Add files via upload

---
+++ 0x01-lockboxes/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    b_keys = set()
+    b_unopened = set()
+    
+    for j in range(2):
+        for i in range(len(boxes)):
+            
+            if i == 0 :
+                b_keys.update(boxes[0])
+            else:
+                if i in b_keys:
+                    b_keys.update(boxes[i])
+                elif j == 1:
+                    b_unopened.update([i])
+    return len(b_keys & b_unopened) == len(b_unopened)
+
+if __name__ == "__main__":
+    canUnlockAll([[1],[0]])
