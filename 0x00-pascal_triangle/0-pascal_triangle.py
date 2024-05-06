5a303b81a26ca07c2c2dc02b9de21d83958a4b57
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-05-06 02:04:12 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """ pascal_triangle """
 
+
 def pascal_triangle(n):
     """ pascal_triangle """
     pascal = []
     if n <= 0:
-        return pascal
-    
-    for row in range(1,n+1):
+        return pascal    
+    for row in range(1, n+1):
         i = []
-        for col in range(1,row+1):
+        for col in range(1, row+1):
             if col == 1:
                 i += [1]
             else:
                 try:
-                    i += [ pascal[row-2][col-1]+pascal[row-2][col-2] ]
+                    i += [pascal[row-2][col-1]+pascal[row-2][col-2]]
                 except:
                     i += [1]
         pascal += [i]
