879aa73433424c65c053a6c00e9fa0574f6c4262
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-05-12 00:46:16 +0000

    A proper MinOps algo

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,26 +3,44 @@
 import math
 
 
+def clear_list(list: list, base: int) -> list:
+    """
+    @new_list the new list which will contain factors that are divisible by @base
+    @appended is used to ignore the next two factors that are divisible by @base to reduce number of ops required to reach @n
+    """
+    new_list = []
+    appended = 0
+    for i in list:
+        if i % base == 0:
+            if appended == 0:
+                appended = 2
+                new_list.append(i)
+            else:
+                appended -= 1
+    return new_list
+
 def getPrimeN(n: int) -> list:
     """
-        Return the smallest prime number @p
-        if @n has no prime divider return @n
+        Return the smallest factor number @p
+        if @n has no factor return @n
     """
     primes = []
     for p in range(3, n):
         if n % p == 0:
             primes.append(p)
-    return primes[0] if len(primes) > 0 else n
+    return primes if len(primes) > 0 else [n]
 
 
 def minOperations(n: int) -> int:
     """
         @n number of copies of desired character
-        @base is the prime number with which we will keep adding
+        @base is the factor which we will keep adding
             to @chars to reach @n
         @ops number of operations done
     """
-    base = getPrimeN(n)
+    base_list = getPrimeN(n)
+    base = base_list.pop(0)
+    base_list = clear_list(base_list, base)
 
     if n <= 1 or math.isinf(n):
         return 0
@@ -35,6 +53,12 @@ def minOperations(n: int) -> int:
     while chars != n:
         ops += 1
         chars += base
+        if len(base_list) >= 1 :
+            if base_list[0] < chars:
+                base_list.pop(0)
+            elif base_list[0] == chars:
+                ops += 1
+                base = base_list.pop(0)
 
     if chars == base:
         return ops
