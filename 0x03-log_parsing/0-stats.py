8aab814990c9fe70f363fb4ac235d3a5e8a2a778
Al-27 <ahmedlaghfour@gmail.com>
2024-05-16 14:50:59 -0700

    chmod

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,9 +22,11 @@ import signal
 
 
 """
+
+
 def ctrl_c(sig, frame):
     print_stats()
-    
+
 
 status_codes = {}
 total_size = 0
@@ -32,38 +34,38 @@ total_size = 0
 
 def print_stats():
     print(f"File size: {total_size}")
-    for k,v in status_codes.pairs():
+    for k, v in status_codes.pairs():
         if v != 0:
             print(f"{k}: {v}")
-    
+
 
 signal.signal(signal.SIGINT, ctrl_c)
 
 
-for code in [ 200, 301, 400, 401, 403, 404, 405 , 500]:
-    status_codes.update({str(code):0})
+for code in [200, 301, 400, 401, 403, 404, 405, 500]:
+    status_codes.update({str(code): 0})
 
 lines_processed = 0
 for line in fileinput.input():
-    pattern = re.compile(r'^(\d+\.){3}\d+ - \[(\d+-){2}\d+ (\d+:){2}\d+.\d+\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$')
- 
+    pattern = re.compile(
+        r'^(\d+\.){3}\d+ - \[(\d+-){2}\d+ (\d+:){2}\d+.\d+\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$')
 
-    if(pattern.findall(line)):
-        match = [ g for g in pattern.findall(line)[0]]
+    if (pattern.findall(line)):
+        match = [g for g in pattern.findall(line)[0]]
     else:
         continue
     # match 0 -> Status Code
     # match 1 -> File Size
     for i in range(3):
         match.pop(0)
-    
+
     i = 1
-    if(len(match) == 2):
-        status_codes[match[0]] +=1
+    if (len(match) == 2):
+        status_codes[match[0]] += 1
     else:
         i = 0
     total_size += match[i]
-    lines_processed +=1
-    
-    if( lines_processed % 10 == 0):
+    lines_processed += 1
+
+    if (lines_processed % 10 == 0):
         print_stats()
