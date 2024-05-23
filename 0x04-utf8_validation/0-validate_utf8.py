ac0f0f83d37fec75ac6fafb5d8f5fac402b03595
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-05-23 03:55:59 +0000

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,8 +5,11 @@ import chardet
 
 def validUTF8(data):
     if type(data) is str:
-        return chardet.detect(bytearray(data,"utf-8"))['encoding'] in ("utf-8", "ascii")
-    return chardet.detect(bytearray(data))['encoding'] in ("utf-8", "ascii")
-
+        return chardet.detect(bytearray(data,"utf-8"))['encoding'] \
+        in ("utf-8", "ascii")
+    try:
+        return chardet.detect(bytearray(data))['encoding'] in ("utf-8", "ascii")
+    except:
+        return False
 if __name__ == "__main__":
     print(validUTF8("wxcdsfvds"))
