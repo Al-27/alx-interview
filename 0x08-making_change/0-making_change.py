fed1e2dda2c16d57ee96ee05244399c67010548e
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-07-26 00:11:14 +0000

    0x08

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+
+def makeChange(coins, total):
+    """mkCH
+    """
+    if total <= 0:
+        return 0
+    # eg: if @total is odd and @coins contains no odd number then
+    # it's unsolvable
+    if total % 2 not in [e % 2 for e in coins] or len(coins) == 0:
+        return -1
+    coins.sort(reverse=True)
+    coins_req = 0
+    i = 0
+    while (total != 0):
+        coin = coins[i]
+        if total < coin:
+            i += 1
+            continue
+
+        total = total - coin
+        coins_req += 1
+    return coins_req
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,2 @@
+# Coin Change
+ least amount of coins required to reach sum X
