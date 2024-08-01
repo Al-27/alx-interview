de143b7c3528afbe9b5afae0af3d2e82a145054e
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-08-01 21:01:16 +0000

    0x09 CS

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+
+
+def island_perimeter(grid):
+    perim = 0
+    MAX_X, MAX_Y = len(grid[0]), len(grid)
+
+    for Y in range(0, MAX_Y):
+        for X in range(0, MAX_X):
+            x1 = X + 1
+            y1 = Y + 1
+            if (x1 < MAX_X):
+                if (grid[Y][x1] != grid[Y][X]):
+                    perim += 1
+            if (y1 < MAX_Y):
+                if (grid[y1][X] != grid[Y][X]):
+                    perim += 1
+    return perim
