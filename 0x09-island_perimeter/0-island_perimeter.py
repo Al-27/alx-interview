1fdac70db93d526a2c9b43367ac8699fb44847e1
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-09-23 18:45:34 +0000

    autopep

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,31 +3,32 @@
 0-main
 """
 
+
 def island_border(grid, x, y, borders):
-    if grid[y][x] == 0 :
-        borders.append({'x':x,'y':y})
+    if grid[y][x] == 0:
+        borders.append({'x': x, 'y': y})
         return 1
     return 0
+
 
 def island_perimeter(grid):
     """Doc
     """
     perim = 0
 
-    #stores the coords of each border as {x,y}, to avoid counting it more than once .
+    # stores the coords of each border as {x,y}, to avoid counting it more
+    # than once .
     borders = []
 
-    #grid[y][x]
+    # grid[y][x]
     MAX_X, MAX_Y = len(grid[0]), len(grid)
-
-    
 
     for Y in range(0, MAX_Y):
         for X in range(0, MAX_X):
-            if grid[Y][X] == 1 :
+            if grid[Y][X] == 1:
                 x, y = X, Y
-                x = [x,x+1,x-1]
-                y = [y,y+1,y-1]
+                x = [x, x + 1, x - 1]
+                y = [y, y + 1, y - 1]
 
                 for _x in x:
                     if _x >= MAX_X:
@@ -41,17 +42,5 @@ def island_perimeter(grid):
                     elif _y < 0:
                         _y = MAX_Y - 1
                     perim += island_border(grid, x[0], _y, borders)
-                    
+
     return perim
-
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
