c2ef8f079dc0b5cbcf50b83ee9d29f4a22a778ed
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-10-04 02:20:17 +0000

    effecient for larger sets but at cost of smaller ones

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,16 +3,22 @@
 DOC
 """
 
+# given that n can be <= 10000, we go overboard and prepare the a primes list
+import timeit
 
-def isPrime(n):
-    """
-    is n prime num
-    """
-    primes = 0
-    for d in range(1, n):
-        if n % d == 0:
-            primes += 1
-    return primes == 1
+
+def primeit():
+    primes = []
+    for n in range(1, 10000):
+        divided = 0
+        for d in range(1, n):
+            if n % d == 0:
+                divided += 1
+            if divided >= 2:
+                break
+        if divided == 1:
+            primes += [n]
+    return primes
 
 
 def isWinner(x, nums):
@@ -20,28 +26,35 @@ def isWinner(x, nums):
 
     # +1 for Ben, -1 for Marian, 0 draw
     score = 0
-
+    primes = None
     for r in range(x):
         score_r = 0  # same as @score but per round
         # 1 Ben's turn, 0 Maria's turn
         turn = False
-
         if nums[r] == 1:
             score += 1
             continue
-        for n in range(nums[r], 0, -1):
+
+        # to ensure we call primeit only once
+        if primes is None:
+            primes = primeit()
+
+        # we extract primal number within nums[r] by intersecting the sets
+        round_set = range(nums[r], 0, -1)
+        round_set = list(set(primes) & set(round_set))
+
+        for n in round_set:
             if n == 1:
                 if turn:
                     score_r -= 1
                 else:
                     score_r += 1
                 break
-            if isPrime(n):
-                if turn:
-                    score_r += 1
-                else:
-                    score_r -= 1
-                turn = not turn
+            if turn:
+                score_r += 1
+            else:
+                score_r -= 1
+            turn = not turn
 
         if score_r > 0:
             score += 1
